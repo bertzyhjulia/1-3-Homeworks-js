ccaf0a503ab15f7cf72ff0fb721e5f2fa84cf56e
julia <90096158+bertzyhjulia@users.noreply.github.com>
2021-09-21 18:46:00 +0300

    homework

---
+++ firstHomework.js
@@ -0,0 +1,13 @@
+let pi = Math.PI.toFixed(2);
+console.log(pi);
+let max = Math.max(15,11,16,23,11,54,87,3,2);
+console.log(max);
+let rand = Math.random();
+console.log(rand);
+let random = Math.random()*10;
+console.log(random);
+let a = (1.6+1.3).toFixed(1);
+console.log(a);
+let num = '100$';
+num = parseFloat('100$');
+console.log(num);
---
+++ thirdHomevork.js
@@ -0,0 +1,9 @@
+const object = {
+    prodbct : "iphone"
+};
+object.price = 1000;
+object.currently = "dolar";
+object.details = {};
+object.details.model = "ios";
+object.details.color = "red";
+console.log(object);
